9dcec1ae5416b60f4a8ac0b6ad044be447186c58
Mathias Lüer <mathias.lueer@etailer.de>
2026-03-21 22:40:22 +0100

    Add doc comments to views_factory

--- nanoservices/todo/networking/actix_server/src/api/mod.rs
+++ nanoservices/todo/networking/actix_server/src/api/mod.rs
@@ -2,6 +2,10 @@ pub mod basic_actions;
 
 use actix_web::web::ServiceConfig;
 
+/// Registers all view factories with the Actix app.
+///
+/// # Arguments
+/// - `app`: A mutable reference to the `ServiceConfig` to register routes on.
 pub fn views_factory(app: &mut ServiceConfig) {
     basic_actions::basic_actions_factory(app);
 }
